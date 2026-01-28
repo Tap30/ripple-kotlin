00be1f1055a2821a8e2d6c15826c9ddd04295ce1
Behnam Banaei <behnam.banaei@tapsi.cab>
2026-01-28 15:58:25 +0330

    make the code ready for gitlab artifactory publishing

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 
 repositories {
     maven {
-        url = uri("https://maven.myket.ir")
-    }
-    maven {
         credentials {
             username = System.getenv("ARTIFACTORY_ANDROID_USERNAME")
             password = System.getenv("ARTIFACTORY_ANDROID_PASSWORD")
         }
         url = uri("https://artifactory.tapsi.tech/artifactory/android-gradle-maven")
+    }
+    maven {
+        url = uri("https://maven.myket.ir")
     }
     gradlePluginPortal()
     mavenCentral()
